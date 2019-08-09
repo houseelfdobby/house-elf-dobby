b0d92fc69f659498c32c2a9f329bbbe0f0167bdb
CaesiumY <mn0658@naver.com>
2019-08-10 04:49:25 +0900

    chore: Deploy heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dobbyproject.wsgi --log-file -
--- dobbyproject/settings.py
+++ dobbyproject/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b+bd1@cs9t-$#ngql%lj-v5g7m^zlm0)*d75su-a2f*caphcc+'
+# SECRET_KEY = 'b+bd1@cs9t-$#ngql%lj-v5g7m^zlm0)*d75su-a2f*caphcc+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'b+bd1@cs9t-$#ngql%lj-v5g7m^zlm0)*d75su-a2f*caphcc+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,9 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
